28aeaa682b456142b2ed29fbbc4455ca165d1794
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-11-10 07:49:01 +0100

    ci: Replace CDK with CDKTF

--- bin/just-code.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { JustCodeStackDev } from '../lib/just-code-stack-dev';
-
-const app = new cdk.App();
-
-// new JustCodeStackDev(app, 'JustCodeStackDev', { env: { region: process.env.AWS_REGION, account: process.env.AWS_ACCOUNT, } });
-new JustCodeStackDev(app, 'JustCodeStackDev', { env: { region: 'eu-central-1', account: '269175870005', } });

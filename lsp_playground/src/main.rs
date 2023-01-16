90a11021f0442e4b3ac3dab1b694a2f77b6f4226
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2023-01-16 14:01:18 +0100

    feat: poc lsp integration

--- lsp_playground/src/main.rs
+++ lsp_playground/src/main.rs
@@ -1,8 +1,8 @@
-use std::process::Stdio;
+use std::{process::Stdio, time::Instant};
 
 use serde_json::json;
 use tokio::{
-    io::{AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader, BufWriter},
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader, BufWriter},
     process::{ChildStderr, ChildStdout, Command},
     sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
 };
@@ -10,11 +10,7 @@ use tokio::{
 #[tokio::main]
 async fn main() {
     let mut process = Command::new("dart")
-        .args([
-            "language-server",
-            "--protocol=analyzer",
-            "--protocol-traffic-log=/Users/mledwon/Projects/just_code/logs.txt",
-        ])
+        .args(["language-server"])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -29,17 +25,50 @@ async fn main() {
     let reader = BufReader::new(process.stdout.take().expect("Failed to open stdout"));
     let stderr = BufReader::new(process.stderr.take().expect("Failed to open stderr"));
 
-    let (tx_client, mut _rx) = unbounded_channel::<Vec<u8>>();
+    let (tx_client, mut rx) = unbounded_channel::<Vec<u8>>();
     let (tx, rx_client) = unbounded_channel::<Vec<u8>>();
 
     let wait = setup_listeners(reader, writer, stderr, rx_client, tx_client);
 
-    let request1 =
-        json!({"id":"1","method":"server.setSubscriptions","params":{"subscriptions":["STATUS"]}});
-    // let request2 = json!({"id":"2","method":"analysis.setAnalysisRoots","params":{"included":["/Users/mledwon/Projects/flutter_base_project"],"excluded":[]}});
+    let request1 = json!({"jsonrpc":"2.0","id":1,"method":"initialize","params":{"processId":null,"rootUri":"file:///Users/mledwon/Projects/flutter_base_project","capabilities":{"textDocument":{"publishDiagnostics": {"relatedInformation": true}}},"trace":"off","workspaceFolders":null,"clientInfo":{"name":"My LSP Client","version":"1.0.0"}}});
+    let request2 = json!({"jsonrpc":"2.0","id":1,"method":"initialized","params": {}});
 
-    let json_bytes = serde_json::to_vec(&request1).unwrap();
-    tx.send(json_bytes).unwrap();
+    let json_bytes = serde_json::to_string(&request1).unwrap();
+    let json_bytes2 = serde_json::to_string(&request2).unwrap();
+    tx.send(
+        format!(
+            "Content-Length: {}\r\n\r\n{}\r\n",
+            serde_json::to_vec(&request1).unwrap().len(),
+            json_bytes
+        )
+        .as_bytes()
+        .to_vec(),
+    )
+    .unwrap();
+
+    tokio::spawn(async move {
+        rx.recv().await.unwrap();
+        let start = Instant::now();
+
+        tx.send(
+            format!(
+                "Content-Length: {}\r\n\r\n{}\r\n",
+                serde_json::to_vec(&request2).unwrap().len(),
+                json_bytes2
+            )
+            .as_bytes()
+            .to_vec(),
+        )
+        .unwrap();
+
+        loop {
+            let result = String::from_utf8(rx.recv().await.unwrap()).unwrap();
+            if result.contains("main.dart") {
+                let duration = start.elapsed();
+                println!("Duration: {:?}", duration);
+            }
+        }
+    });
 
     wait.await;
 }
@@ -74,10 +103,10 @@ async fn recv_error(mut reader: BufReader<ChildStderr>) {
 
 async fn recv(mut reader: BufReader<ChildStdout>, tx: UnboundedSender<Vec<u8>>) {
     loop {
-        let mut line = String::new();
-        reader.read_line(&mut line).await.unwrap();
-        println!("Received: {}", line);
-        tx.send(line.as_bytes().to_vec()).unwrap();
+        let mut buffer = String::new();
+        let result = recv_server_message(&mut reader, &mut buffer).await;
+        println!("Received: {}", result);
+        tx.send(result.as_bytes().to_vec()).unwrap();
     }
 }
 
@@ -100,4 +129,48 @@ async fn send_to_server(writer: &mut (impl AsyncWrite + Unpin), data: &[u8]) {
     println!("Content-lenght: {}", data.len());
     writer.write_all(data).await.unwrap();
     writer.flush().await.unwrap();
+}
+
+async fn recv_server_message(reader: &mut BufReader<ChildStdout>, buffer: &mut String) -> String {
+    let mut content_length = None;
+    loop {
+        buffer.truncate(0);
+        if reader.read_line(buffer).await.unwrap() == 0 {
+            panic!();
+        };
+
+        // debug!("<- header {:?}", buffer);
+
+        if buffer == "\r\n" {
+            // look for an empty CRLF line
+            break;
+        }
+
+        let header = buffer.trim();
+
+        let parts = header.split_once(": ");
+
+        match parts {
+            Some(("Content-Length", value)) => {
+                content_length = Some(value.parse().unwrap());
+            }
+            Some((_, _)) => {}
+            None => {
+                // Workaround: Some non-conformant language servers will output logging and other garbage
+                // into the same stream as JSON-RPC messages. This can also happen from shell scripts that spawn
+                // the server. Skip such lines and log a warning.
+
+                // warn!("Failed to parse header: {:?}", header);
+            }
+        }
+    }
+
+    let content_length = content_length.unwrap();
+
+    //TODO: reuse vector
+    let mut content = vec![0; content_length];
+    reader.read_exact(&mut content).await.unwrap();
+    let msg = std::str::from_utf8(&content).unwrap();
+
+    msg.to_string()
 }
